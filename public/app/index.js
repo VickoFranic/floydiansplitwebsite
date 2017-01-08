d980d3dfa6b5c6c8c9a50719f74dd2f8b27df625
Vicko Franić <vfranic@gmail.com>
2017-01-08 15:22:01 +0100

    Folders reorganized

---
+++ public/app/index.js
@@ -0,0 +1,48 @@
+$(window).load(function() {
+
+	var eventData = JSON.parse( $('input[name="event"]').val() );
+
+	$.notify({
+		icon: 'https://scontent.xx.fbcdn.net/v/t1.0-1/c7.99.706.706/s50x50/1504041_766540203358827_4627077962019328691_n.jpg?oh=c5432ed93c4a2432f55108413e0c36fb&oe=5923A975',
+		title: buildTitle(eventData),
+		message: buildMessage(eventData),
+		url: 'https://facebook.com/floydiansplit/events',
+		target: '_blank'
+	},{
+		type: 'minimalist',
+		delay: 10000,
+		icon_type: 'image',
+		placement: {
+			from: "top",
+			align: "right"
+		},
+		template: '<div data-notify="container" class="col-sm-4 col-sm-offset-6 alert alert-{0}">' +
+					'<img data-notify="icon" class="img-circle pull-right"><br>' +
+					'<p><strong data-notify="title">{1}</strong></p>' +
+					'<p><small>{2}</small></p>' +
+					'<a href="{3}" target="{4}" data-notify="url"></a>' +
+				'</div>'
+	});
+
+	function buildTitle(data) {
+		moment.locale('hr');
+		var dt = moment(data.start_time).format('LLLL');
+
+		var title = 
+			'<h5>' + data.name + '</h5>' +
+			'<br><span>' + dt + '</span>';
+
+		return title;
+	}
+
+	function buildMessage(data) {
+
+		var message = 
+			'<span class="glyphicon glyphicon-map-marker"></span>' +
+			'<br>' + data.place.name + 
+			'<br>' + data.place.location.street + 
+			'<br>' + data.place.location.city;
+
+		return message;
+	}
+});
---
+++ src/App.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace FloydianSplit;
+
+use Facebook\Facebook;
+use Facebook\Exceptions\FacebookResponseException;
+use Facebook\Exceptions\FacebookSDKException;
+use \Twig_Loader_Filesystem;
+use \Twig_Environment;
+
+/**
+* App
+*/
+class App
+{
+	/**
+	 * Facebook
+	 * @param Facebook $fb
+	 */
+	public $fb;
+
+	/**
+	 * Twig
+	 * @param Twig $twig
+	 */
+	public $twig;
+
+	public function __construct()
+	{
+		$this->fb = new Facebook([
+		  'app_id' => APP_ID,
+		  'app_secret' => APP_SECRET,
+		  'default_graph_version' => 'v2.5',
+		]);
+
+		$this->fb->setDefaultAccessToken(ACCESS_TOKEN);
+
+		$this->twig = $this->loadTwig();
+	}
+
+	/**
+	 * Using Twig as template enginge
+	 * @return Twig
+	 */
+	private function loadTwig() 
+	{
+		$loader = new Twig_Loader_Filesystem('./public/templates/');
+		$twig = new Twig_Environment($loader, array(
+		    'cache' => TWIG_CACHE_PROD,
+		));
+
+		return $twig;
+	}
+
+	/**
+	 * Read last event data for Floydian Split from Facebook API
+	 * @return GraphNode | last event node
+	 */
+	public function getLastEventForFloydians()
+	{
+		try {
+		  $response = $this->fb->get('floydiansplit/events?fields=name,start_time,place&limit=1');
+
+		} catch(FacebookResponseException $e) {
+			$f = fopen("graph_error.txt", "a");
+			fwrite($f, 'Graph returned an error: ' . $e->getMessage() . PHP_EOL);
+			fclose($f);
+		} catch(FacebookSDKException $e) {
+			$f = fopen("fb_sdk_error.txt", "a");
+			fwrite($f, 'Facebook SDK returned an error: ' . $e->getMessage() . PHP_EOL);
+			fclose($f);
+		}
+
+		// Success
+		$event = $response->getGraphEdge()->map(function($data) {
+			return $data;
+		});
+
+		return $event[0];
+	}
+}
