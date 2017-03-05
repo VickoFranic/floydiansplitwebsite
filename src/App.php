4fde0d910d143fdf7eeaf2d0f5cdf2911ca0423f
Vicko Franić <vicko@q-alliance.com>
2017-03-05 13:18:02 +0100

    Twig auto reload, minor css updated

--- src/App.php
+++ src/App.php
@@ -30,6 +30,7 @@ class App
 		$loader = new Twig_Loader_Filesystem('./public/templates/');
 		$twig = new Twig_Environment($loader, array(
 		    'cache' => './cache',
+		    'auto_reload' => true
 		));
 
 		return $twig;
